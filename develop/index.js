07487a10c275a2e8d6588657cc3ccfe312d2d8be
Shalah <zoungranaala@gmail.com>
2022-02-09 21:56:48 -0500

    Added few questions to the array and also a couple of modules

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'name',
+        },
 
-// TODO: Create a function to initialize app
-function init() {}
+        {
+            type: 'input',
+            message: 'What is your email?',
+            email: 'email',
+        },
 
-// Function call to initialize app
-init();
+        {
+            type: 'input',
+            message: 'What is your project name?',
+            project: 'projectName',
+        },
+
+        {
+            type: 'list',
+            message: 'What kind of license should your project have?',
+            name: 'language',
+            choices: ["English", "French", "Spanish"]
+        },
+
+        {
+            type: 'list',
+            message: 'What is your preferred method of communication?',
+            name: 'Communication',
+            choices: ["Phone", "Email", "In-Person"]
+        }
+    ])
+        
+        .then((response) => {
+            console.log(response);
+            // fs.writeFile('userentries.txt', JSON.stringify(response), function (err) {
+            //     if (err) {
+            //         console.log(err)
+            //     }
+            //     else {
+            //         console.log("Successful!!")
+            //     }
+            // });
+        })
+
+
+
+
+]    
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init()
