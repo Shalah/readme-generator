9128896e824b9361c57e73f203215f599ca58a42
Shalah <zoungranaala@gmail.com>
2022-02-10 10:59:55 -0500

    Added a mockup README to the index.js

--- develop/index.js
+++ develop/index.js
@@ -1,60 +1,10 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { features } = require('process');
 
 // TODO: Create an array of questions for user input
-const questions = [
-
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'name',
-        },
-
-        {
-            type: 'input',
-            message: 'What is your email?',
-            email: 'email',
-        },
-
-        {
-            type: 'input',
-            message: 'What is your project name?',
-            project: 'projectName',
-        },
-
-        {
-            type: 'list',
-            message: 'What kind of license should your project have?',
-            name: 'language',
-            choices: ["English", "French", "Spanish"]
-        },
-
-        {
-            type: 'list',
-            message: 'What is your preferred method of communication?',
-            name: 'Communication',
-            choices: ["Phone", "Email", "In-Person"]
-        }
-    ])
-        
-        .then((response) => {
-            console.log(response);
-            // fs.writeFile('userentries.txt', JSON.stringify(response), function (err) {
-            //     if (err) {
-            //         console.log(err)
-            //     }
-            //     else {
-            //         console.log("Successful!!")
-            //     }
-            // });
-        })
-
-
-
-
-]    
+ 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
@@ -63,3 +13,41 @@ const questions = [
 
 // // Function call to initialize app
 // init()
+
+
+
+
+
+const generateHTML = ({ projectTitle, description, installation, usage, license, features, Contributions }) =>
+  `# ${projectTitle}
+
+  ## ${description}
+    
+  
+  ## Table of Contents (Optional)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+  
+  
+  ## ${installation}
+  
+  
+  ## ${usage}
+  
+  
+  ## ${license}
+  
+  
+  ## ${features}
+  
+  
+  ## ${Contributions}
+  
+  
+`;
+
+
+
+
